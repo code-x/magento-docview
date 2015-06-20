d70e41fef056ca072355d7b20d0ecf064e723b89
Tobias Vogt <tobias@code-x.de>
2015-06-20 17:21:47 +0200

    fix: do not validate formkey

--- app/code/local/Codex/Docview/controllers/Adminhtml/DocviewController.php
+++ app/code/local/Codex/Docview/controllers/Adminhtml/DocviewController.php
@@ -2,6 +2,16 @@
 
 class Codex_Docview_Adminhtml_DocviewController extends Mage_Adminhtml_Controller_Action
 {
+    protected function _validateFormKey()
+    {
+        return true;
+    }
+
+    protected function _validateSecretKey()
+    {
+       return true;
+    }
+
     public function indexAction()
     {
         $docHelper = Mage::helper('codex_docview');
